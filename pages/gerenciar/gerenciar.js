93691397e530697a368116325f64af1ac04df3cd
David Duarte (Williams) <davidduart04@gmail.com>
2024-07-28 13:02:39 -0300

    primeira versao totalmente funcional

--- pages/gerenciar/gerenciar.js
+++ pages/gerenciar/gerenciar.js
@@ -1,12 +1,69 @@
-function pesquisar() {
+
+const livros = resgatarLivros();
+
+carregarLista();
+
+function salvarLivros(livros) {
+    localStorage.setItem('livros', JSON.stringify(livros));
 }
 
-function editar() {
+function resgatarLivros() {
+    return JSON.parse(localStorage.getItem('livros')) || [];
+}
+
+function carregarLista() {
+    let novaLi = '';
+
+    livros.forEach((livro, index) => {
+        novaLi += `<div class="divLi">
+                    <li>${livro.nome}</li>
+                    <img src="/img/editar.png" id="editar" onclick="editar(${index})">
+                    <img src="/img/apagar.png" id="apagar" onclick="apagar(${index})">
+                </div>`;
+    });
+
+    const lista = document.querySelector('ul');
+    lista.innerHTML = novaLi;
+
+}
+
+function pesquisar() {
+    const nome = document.getElementById("inputPesquisa").value;
+
+    let novaLi = '';
+
+    livros.forEach((livro, index) => {
+
+        if (livro.nome.toLowerCase().includes(nome.toLowerCase())) {
+            novaLi += `<div class="divLi">
+            <li>${livro.nome}</li>
+            <img src="/img/editar.png" id="editar" onclick="editar(${index})">
+            <img src="/img/apagar.png" id="apagar" onclick="apagar(${index})">
+        </div>`;
+        }
+
+    });
+
+    const lista = document.querySelector('ul');
+    lista.innerHTML = novaLi;
+}
+
+function inputVazio() {
+
+    const valorInput = document.getElementById("inputPesquisa").value;
+
+    if (valorInput == '') {
+        carregarLista();
+    }
+
+}
+
+function editar(index) {
     var confirma = `<div id="confirma">
         <div id="confirmaContainer">
-            <input type="text" placeholder="nome do livro">
+            <input type="text" placeholder="nome do livro" id="inputEditarNome" value="${livros[index].nome}">
             <div id="buttons">
-                <button id="Confirmar">Confirmar</button>
+                <button id="Confirmar" onclick="editarLivro(${index})">Confirmar</button>
                 <button id="Cancelar" onclick="cancelar()">Cancelar</button>
             </div>
         </div>
@@ -17,12 +74,24 @@ function editar() {
     document.body.appendChild(containerTemporario.firstElementChild);
 }
 
-function apagar() {
+function editarLivro(index) {
+    const novoNome = document.getElementById("inputEditarNome").value;
+
+    if (novoNome) {
+        livros[index].nome = novoNome;
+        salvarLivros(livros);
+    }
+
+    cancelar();
+    carregarLista();
+}
+
+function apagar(index) {
     var confirma = `<div id="confirma">
         <div id="confirmaContainer">
-            <label for="">Excluir "nome do livro"?</label>
+            <label for="">Excluir "${livros[index].nome}"?</label>
             <div id="buttons">
-                <button id="Confirmar">Confirmar</button>
+                <button id="Confirmar" onclick = "excluir(${index})">Confirmar</button>
                 <button id="Cancelar" onclick = "cancelar()">Cancelar</button>
             </div>
         </div>
@@ -31,6 +100,13 @@ function apagar() {
     var containerTemporario = document.createElement('div');
     containerTemporario.innerHTML = confirma;
     document.body.appendChild(containerTemporario.firstElementChild);
+}
+
+function excluir(index) {
+    livros.splice(index, 1);
+    salvarLivros(livros);
+    cancelar();
+    carregarLista();
 }
 
 function cancelar() {
@@ -43,9 +119,9 @@ function cancelar() {
 function novo() {
     var confirma = `<div id="confirma">
         <div id="confirmaContainer">
-            <input type="text" placeholder="nome do livro">
+            <input type="text" placeholder="nome do livro" id = "inputNomeLivro">
             <div id="buttons">
-                <button id="Confirmar">Confirmar</button>
+                <button id="Confirmar" onclick = "inserirLivro()">Confirmar</button>
                 <button id="Cancelar" onclick="cancelar()">Cancelar</button>
             </div>
         </div>
@@ -55,3 +131,44 @@ function novo() {
     containerTemporario.innerHTML = confirma;
     document.body.appendChild(containerTemporario.firstElementChild);
 }
+
+function inserirLivro() {
+    const nomeLivro = document.getElementById("inputNomeLivro").value;
+
+    if (nomeLivro) {
+        const novoLivro = {
+            nome: nomeLivro,
+            disponivel: true
+        };
+
+        livros.push(novoLivro);
+        salvarLivros(livros);
+    }
+    carregarLista();
+    cancelar();
+}
+
+/*
+function novoLivro() {
+    const nomeLivro = document.getElementById("inputNomeLivro").value;
+    const novoLivro = {
+        nome: nomeLivro,
+        disponivel: true
+    };
+
+    livros.push(novoLivro);
+
+    cancelar();
+
+    var newLi = `<div class="divLi">
+                    <li>${novoLivro.nome}</li>
+                    <img src="/img/editar.png" id="editar" onclick="editar()">
+                    <img src="/img/apagar.png" id="apagar" onclick="apagar()">
+                </div>`;
+
+    var ul = document.querySelector('ul');
+    var containerTemporario = document.createElement('div');
+    containerTemporario.innerHTML = newLi;
+    ul.appendChild(containerTemporario.firstElementChild);
+}
+    */
--- pages/verLivros/verLivros.js
+++ pages/verLivros/verLivros.js
@@ -1,3 +1,109 @@
+
+const livros = resgatarLivros();
+
+carregarLista();
+
+function salvarLivros(livros) {
+    localStorage.setItem('livros', JSON.stringify(livros));
+}
+
+function resgatarLivros() {
+    return JSON.parse(localStorage.getItem('livros')) || [];
+}
+
+function carregarLista() {
+    let novaLi = '';
+
+    livros.forEach((livro, index) => {
+
+        if (livro.disponivel) {
+            novaLi += `<div class="divLi">
+            <li>${livro.nome}</li>
+            <label class="disponivel" onclick="confirma('Emprestar', ${index})">Disponível</label>
+        </div>`;
+        } else {
+            novaLi += `<div class="divLi">
+            <li>${livro.nome}</li>
+            <label class="indisponivel" onclick="confirma('Devolver',${index})">Indisponível</label>
+        </div>`;
+        }
+
+
+    });
+
+    const lista = document.querySelector('ul');
+    lista.innerHTML = novaLi;
+
+}
+
+function confirma(string, index) {
+    var confirma = `<div id="confirma">
+        <div id="confirmaContainer">
+            <label for="">${string} "${livros[index].nome}"?</label>
+            <div id="buttons">
+                <button id="Confirmar" onclick = "emprestarDevolver(${index})">Confirmar</button>
+                <button id="Cancelar" onclick = "cancelar()">Cancelar</button>
+            </div>
+        </div>
+    </div>`;
+
+    var containerTemporario = document.createElement('div');
+    containerTemporario.innerHTML = confirma;
+    document.body.appendChild(containerTemporario.firstElementChild);
+}
+
+function cancelar() {
+    var confirma = document.getElementById("confirma");
+    if (confirma) {
+        confirma.remove();
+    }
+}
+
+
+function emprestarDevolver(index) {
+    livros[index].disponivel = !livros[index].disponivel;
+    carregarLista();
+    cancelar();
+    salvarLivros(livros);
+}
+
+function pesquisar() {
+    const nome = document.getElementById("inputPesquisa").value;
+
+    let novaLi = '';
+
+    livros.forEach((livro, index) => {
+
+        if (livro.nome.toLowerCase().includes(nome.toLowerCase())) {
+            if (livro.disponivel) {
+                novaLi += `<div class="divLi">
+                <li>${livro.nome}</li>
+                <label class="disponivel" onclick="confirma('Emprestar', ${index})">Disponível</label>
+            </div>`;
+            } else {
+                novaLi += `<div class="divLi">
+                <li>${livro.nome}</li>
+                <label class="indisponivel" onclick="confirma('Devolver',${index})">Indisponível</label>
+            </div>`;
+            }
+        }
+    });
+
+    const lista = document.querySelector('ul');
+    lista.innerHTML = novaLi;
+}
+
+function inputVazio() {
+
+    const valorInput = document.getElementById("inputPesquisa").value;
+
+    if (valorInput == '') {
+        carregarLista();
+    }
+
+}
+
+/*
 function pesquisar() {
 }
 
@@ -39,3 +145,4 @@ function cancelar() {
         confirma.remove();
     }
 }
+*/
